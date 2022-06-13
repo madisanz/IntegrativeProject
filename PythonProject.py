efd5c7a9e4ecb205daadce67c8c5a08d3e20ebfa
Margarita Sanchez <madisanz1@gmail.com>
2022-06-12 23:40:11 -0300

    First Commit

---
+++ PythonProject.py
@@ -0,0 +1,30 @@
+'''pythonproject.py
+Codificar en Python un programa que contenga las siguientes condiciones:
+
+   -> El usuario debe ingresar 5 números enteros, los cuales serán almacenados en una
+lista
+   -> Función Suma
+   -> Función Promedio
+   -> Función Máximo
+   -> Función Mínimo
+
+Grupo: 
+   -> Margarita Sánchez     E-mail: madisanz1@gmail.com
+   -> Carla Argentina Wayar E-mail: carlaargentinawayar@gmail.com
+   -> Vanesa Paola Soria    E-mail: vanesa.soria.p@gmail.com
+   ->
+   ->
+   
+'''
+
+
+#Definimos una lista vacia
+lista=[]
+
+#El usuario ingresa los 5 numeros enteros
+for x in range(5):
+    valor=int(input("Ingrese un valor entero:"))
+    lista.append(valor)
+
+#Imprimimos la lista
+print(lista)
